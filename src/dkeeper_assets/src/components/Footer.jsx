5143469f310785d743f0eb3412bd5489234ccd0b
Leonardo Genzolini <leonardogenzolini@gmail.com>
2023-07-09 18:49:31 +0200

    css final touch

--- src/dkeeper_assets/src/components/Footer.jsx
+++ src/dkeeper_assets/src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p>ⓒ Leonardo Genzolini {year}</p>
     </footer>
   );
 }
